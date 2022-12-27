4607b2c327332570a533dfb86a360eb73874acd4
khanhson261 <61035327+khanhson261@users.noreply.github.com>
2022-12-27 15:45:35 +0700

    initial commit
    add initial commit

---
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RMEpisodeViewController.swift
+//  RickAndMorty
+//
+//  Created by Khanh Son on 12/27/22.
+//
+
+import UIKit
+
+final class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Episodes"
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RickAndMorty/Controllers/Core/RMTabBarViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Khanh Son on 12/27/22.
+//
+
+import UIKit
+
+final class RMTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setUpTabs()
+    
+    }
+
+    private func setUpTabs() {
+        let characterVC = RMCharacterViewController()
+        let locationVC = RMLocationViewController()
+        let episodeVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+       
+        characterVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodeVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: characterVC)
+        let nav2 = UINavigationController(rootViewController: locationVC)
+        let nav3 = UINavigationController(rootViewController: episodeVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "film.stack"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
+}
+
